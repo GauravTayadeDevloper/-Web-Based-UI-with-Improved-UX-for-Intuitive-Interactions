878f0124e0d9556bf6a614d7e3eaa378f330dfa0
GauravTayadeDevloper <gauravtayade619@gmail.com>
2025-12-31 18:50:53 +0530

    ui mobile view ui bugs fixed improvements

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,12 +1,24 @@
-import { Link } from "react-router-dom";
-import './Header.css'
+import { Link, useLocation } from "react-router-dom";
+import "./Header.css";
 export default function Navbar() {
+  const location = useLocation();
+  const isHome = location.pathname === "/";
   return (
-       <nav className="nav">
-        <Link to="/" className="nav-item">Home</Link>
-          <Link to="/skeleton" className="nav-item">Skeleton View</Link>
-          <Link to="/texture" className="nav-item">Textured View</Link>
-          <Link to="/fps" className="nav-item">FPS View</Link>
-        </nav>
+    <nav className="nav">
+      {!isHome && (
+        <Link to="/" className="nav-item">
+          Home
+        </Link>
+      )}
+      <Link to="/skeleton" className="nav-item">
+        Skeleton View
+      </Link>
+      <Link to="/texture" className="nav-item">
+        Textured View
+      </Link>
+      <Link to="/fps" className="nav-item">
+        FPS View
+      </Link>
+    </nav>
   );
 }
